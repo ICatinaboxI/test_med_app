d7e479bc599e4596af596d82544695aca27b207c
ICatinaboxI <s443016623@st.uqu.edu.sa>
2024-08-16 13:05:44 -0400

    initial commit

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -3,59 +3,53 @@ import { Link } from "react-router-dom";
 
 import "./Navbar.css";
 
-
-
 const Navbar = () => {
-    const [click, setClick] = useState(false);
+  const [click, setClick] = useState(false);
 
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
-    const [username, setUsername] = useState("");
-    const[email,setEmail]=useState("");
-    const [showDropdown, setShowDropdown] = useState(false);
-    const handleClick = () => setClick(!click);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [showDropdown, setShowDropdown] = useState(false);
+  const handleClick = () => setClick(!click);
 
-    
-    const handleLogout = () => {
-        sessionStorage.removeItem("auth-token");
-        sessionStorage.removeItem("name");
-        sessionStorage.removeItem("email");
-        sessionStorage.removeItem("phone");
-        // remove email phone
-        localStorage.removeItem("doctorData");
-        setIsLoggedIn(false);
-       
-        // Remove the reviewFormData from local storage
-        for (let i = 0; i < localStorage.length; i++) {
-          const key = localStorage.key(i);
-          if (key.startsWith("reviewFormData_")) {
-            localStorage.removeItem(key);
-          }
-        }
-        setEmail('');
-        window.location.reload();
+  const handleLogout = () => {
+    sessionStorage.removeItem("auth-token");
+    sessionStorage.removeItem("name");
+    sessionStorage.removeItem("email");
+    sessionStorage.removeItem("phone");
+    localStorage.removeItem("doctorData");
+    setIsLoggedIn(false);
+    setEmail("");
+    setUsername("");
+    window.location.reload();
+  };
+
+  const handleDropdown = () => {
+    setShowDropdown(!showDropdown);
+  };
+
+  useEffect(() => {
+    const storedName = sessionStorage.getItem("name");
+    const storedEmail = sessionStorage.getItem("email");
+    if (storedName && storedEmail) {
+      setIsLoggedIn(true);
+      setUsername(storedName);
+      setEmail(storedEmail);
     }
-    const handleDropdown = () => {
-      setShowDropdown(!showDropdown);
-    }
-    useEffect(() => { 
-      const storedemail = sessionStorage.getItem("email");
+  }, []);
 
-      if (storedemail) {
-            setIsLoggedIn(true);
-            setUsername(storedemail);
-          }
-        }, []);
   return (
     <nav>
       <div className="nav__logo">
         <Link to="/">
-        StayHealthy <i style={{color:'#2190FF'}} className="fa fa-user-md"></i></Link>
+          StayHealthy <i style={{ color: "#2190FF" }} className="fa fa-user-md"></i>
+        </Link>
         <span>.</span>
       </div>
       <div className="nav__icon" onClick={handleClick}>
         <i className={click ? "fa fa-times" : "fa fa-bars"}></i>
       </div>
-      <ul className={click ? 'nav__links active' : 'nav__links'}>
+      <ul className={click ? "nav__links active" : "nav__links"}>
         <li className="link">
           <Link to="/">Home</Link>
         </li>
@@ -66,19 +60,19 @@ const Navbar = () => {
           <Link to="/healthblog">Health Blog</Link>
         </li>
         <li className="link">
-         <Link to="/ReviewForm">Reviews</Link>
+          <Link to="/reviews">Reviews</Link>
         </li>
-        <li className="link">
-            <Link to="/InstantConsultation">Instant Consultation</Link>
-        </li>
-        {isLoggedIn?(
+        {isLoggedIn ? (
           <>
+            <li className="link">
+              <span>Weclome, {username}</span>
+            </li>
+
             <li className="link">
               <button className="btn2" onClick={handleLogout}>
                 Logout
               </button>
             </li>
-            
           </>
         ) : (
           <>
